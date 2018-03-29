ea40511e69a2e1944e6001b8d0edfa76e16cfc55
sxiny <748701592@qq.com>
2018-03-29 14:06:46 +0800

    第一次提交memcached

---
+++ src/main/java/com/zpark/memocached/MemocachedApplication.java
@@ -0,0 +1,12 @@
+package com.zpark.memocached;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemocachedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemocachedApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zpark/memocached/Momcached.java
@@ -0,0 +1,15 @@
+package com.zpark.memocached;
+
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+public class Momcached {
+
+    public static void main(String[] args) {
+
+
+        MemcachedClientBuilder memcachedClientBuilder = new XMemcachedClientBuilder(
+                AddrUtil.getAddresses("192.168.100.132:11211"));
+    }
+}
