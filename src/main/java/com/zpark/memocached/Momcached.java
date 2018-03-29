b75d836624a965f7e2515c531ff50dace92f038f
sxiny <748701592@qq.com>
2018-03-29 21:36:45 +0800

    第一次修改
    Signed-off-by: sxiny <748701592@qq.com>

--- src/main/java/com/zpark/memocached/Momcached.java
+++ src/main/java/com/zpark/memocached/Momcached.java
@@ -11,5 +11,6 @@ public class Momcached {
 
         MemcachedClientBuilder memcachedClientBuilder = new XMemcachedClientBuilder(
                 AddrUtil.getAddresses("192.168.100.132:11211"));
+        System.out.println("hahahhahahhahahahha");
     }
 }
